d077272935f856da7f27b9e65946cc5e479d0499
Witold Mrózek <wmrozek89@gmail.com>
2018-04-22 13:29:17 +0200

    Servlet hello

---
+++ kino/src/main/java/com/sda/kino/hello/HelloServlet.java
@@ -0,0 +1,47 @@
+package com.sda.kino.hello;
+
+import sun.java2d.pipe.OutlineTextRenderer;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Enumeration;
+
+
+public class HelloServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletOutputStream out = response.getOutputStream();
+        out.println("Hello Servlet");
+        out.println("Dzisiejsza data: "+ LocalDate.now());
+        out.println(String.format("Jakis tekst powiazany ze zmiennymi %s, %d, %b", "ala", 29384, true));
+        out.println("METODA: "+request.getMethod());
+        out.println("Naglowki");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()){
+            String header = headerNames.nextElement();
+            out.println(header +": "+request.getHeader(header));
+        }
+
+        String param1 = request.getParameter("param1");
+        String[] param1Values = request.getParameterValues("param1");
+        for (String name : param1Values){
+            out.println("Imie: "+name);
+        }
+        String param2 = request.getParameter("param2");
+        String param3 = request.getParameter("param3");
+        out.println(String.format("Wartosci parametrow: %s, %s, %s", param1, param2, param3));
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while(parameterNames.hasMoreElements()){
+            String parameter = parameterNames.nextElement();
+            out.println(parameter +": "+request.getParameter(parameter));
+        }
+    }
+}
