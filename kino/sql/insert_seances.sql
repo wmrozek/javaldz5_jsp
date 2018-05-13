dd078a652c713cc387f20d2a2019d3447fa01bb3
zaleski <s.zaleski88@gmail.com>
2018-05-13 15:19:48 +0200

    dodane query do zmiany i populacji baz danych

---
+++ kino/sql/insert_seances.sql
@@ -0,0 +1,14 @@
+
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (1, '05-13-2018', '12:00', 12);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (1, '05-13-2018', '13:00', 12.00);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (1, '05-13-2018', '16:00', 12.00);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (1, '05-13-2018', '18:00', 15.00);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (2, '06-13-2018', '12:00', 12.00);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (2, '05-13-2018', '12:00', 12.00);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (2, '05-13-2018', '14:00', 14.00);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (3, '06-13-2018', '12:00', 12.00);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (4, '06-13-2018', '18:00', 15.00);
+
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (4, '06-13-2018', '14:00', 12.00);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (5, '05-13-2018', '15:00', 12.00);
+insert into seances(id_movie, data_start, data_start_time, price) VALUES (3, '05-13-2018', '16:00', 12.00);
